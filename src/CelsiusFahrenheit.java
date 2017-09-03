361f2729cc097e2d861add2328ce4fb3bb12b695
mautry16 <31490633+mautry16@users.noreply.github.com>
2017-09-03 17:16:32 -0400

    Update CelsiusFahrenheit.java

--- src/CelsiusFahrenheit.java
+++ src/CelsiusFahrenheit.java
@@ -1,11 +1,3 @@
-/**
- * Chapter 3
- *
- * Have the user input a temperature and if they would like to 
- * Convert to Fahrenheit or Celsius
- *
- * Michael Autry - 9/2/17
- */
 import java.util.Scanner;
 
 public class CelsiusFahrenheit
@@ -21,7 +13,9 @@ public class CelsiusFahrenheit
         while (true)
         {
             System.out.print("\nEnter a temperature: ");
+
             while (!keyboard.hasNextInt())
+
             {
                 keyboard.next();
                 System.out.print("Error. Try Again: ");
@@ -31,18 +25,18 @@ public class CelsiusFahrenheit
             System.out.print("\nWould you like to convert to Celsius 'C' or Fahrenheit 'F': ");
             Type = keyboard.next();
 
-                if (Type.equalsIgnoreCase(Fahrenheit)) {
-                    int C_F = 5 * (temp - 32) / 9;
-                    System.out.print("\n" + temp + " degrees Fahrenheit = " + C_F + " degrees Celsius.");
-                    System.out.println(" ");
-                } else if (Type.equalsIgnoreCase(Celsius)) {
-                    int F_C = (9 * (temp / 5)) + 32;
-                    System.out.print("\n" + temp + " degrees Celsius = " + F_C + " degrees Fahrenheit");
-                    System.out.println(" ");
-                } else {
-                    System.out.print("\nError.");
-                    break;
-                }
+            if (Type.equalsIgnoreCase(Celsius)) {
+                int F_C = 5 * (temp - 32) / 9;
+                System.out.print("\n" + temp + " degrees Fahrenheit = " + F_C + " degrees Celsius.");
+                System.out.println(" ");
+            } else if (Type.equalsIgnoreCase(Fahrenheit)) {
+                int C_F = (9 * (temp / 5)) + 32;
+                System.out.print("\n" + temp + " degrees Celsius = " + C_F + " degrees Fahrenheit");
+                System.out.println(" ");
+            } else {
+                System.out.print("\nError.");
+                break;
+            }
         }
     }
 }
